da618075ec263fb887192ce406ba07866e50e0d0
ihoek <ihoek679@naver.com>
2025-02-13 20:26:06 +0900

    axios 연습

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const express = require("express"); //express 모듈 세팅
+const multer = require("multer");
+const upload = multer({ data: "uploads/" });
+
 const app = express();
 const port = 3000;
 const path = require("path");
 
 let data = "";
 let data_name = "";
-const file_list = [
-  "aaww1334",
-  "asd14123",
-  "asssjsjdj2",
-  "dddw123",
-  "ddjwjwjaa111",
-  "djdwjw1",
-  "djwjaaaw1",
-  "dnansndjw1",
-  "dwdwds1112",
-  "dwdwwd1231",
-  "hshsh12",
-  "jdjsdjkkw1",
-  "jsjdhdjwh1",
-  "like21",
-  "nnsd221",
-  "nodes1",
-  "user12",
-  "user22221",
-  "wdwdwddw1122313",
-  "wdwsss124",
-];
-const reversed = file_list.reverse();
-const userData = { id: "123", pw: "123" };
+
+// const file_list = [
+//   "aaww1334",
+//   "asd14123",
+//   "asssjsjdj2",
+//   "dddw123",
+//   "ddjwjwjaa111",
+//   "djdwjw1",
+//   "djwjaaaw1",
+//   "dnansndjw1",
+//   "dwdwds1112",
+//   "dwdwwd1231",
+//   "hshsh12",
+//   "jdjsdjkkw1",
+//   "jsjdhdjwh1",
+//   "like21",
+//   "nnsd221",
+//   "nodes1",
+//   "user12",
+//   "user22221",
+//   "wdwdwddw1122313",
+//   "wdwsss124",
+// ];
+// const reversed = file_list.reverse();
+// const userData = { id: "123", pw: "123" };
+
 //body parser
 
 // x-www-form-urlencoded 방식, 객체 형태로 결과가 나옴
@@ -36,8 +41,8 @@ app.use(express.urlencoded({ extended: true }));
 //json
 app.use(express.json());
 
-app.use(express.static("static"));
-
+//app.use(express.static("static"));
+app.use("/static", express.static(__dirname + "/static"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -92,28 +97,38 @@ app.get("/axios_test2", (req, res) => {
   res.render("axios_test2");
 });
 // axios_test2 POST 요청 처리
-app.post("/axios_test2", (req, res) => {
-  const { user_id, user_pw } = req.body;
+// app.post("/axios_test2", (req, res) => {
+//   const { user_id, user_pw } = req.body;
 
-  if (user_id === userData.id && user_pw === userData.pw) {
-    res.json({
-      success: true,
-      message: "로그인 성공",
-    });
-  } else {
-    res.json({
-      success: false,
-      message: "로그인 실패",
-    });
-  }
+//   if (user_id === userData.id && user_pw === userData.pw) {
+//     res.json({
+//       success: true,
+//       message: "로그인 성공",
+//     });
+//   } else {
+//     res.json({
+//       success: false,
+//       message: "로그인 실패",
+//     });
+//   }
+// });
+// //버튼
+// for (let i = 0; i < 20; i++) {
+//   //console.log(file_list[i]);
+//   app.get(`/${file_list[i]}`, function (req, res) {
+//     res.render(`${reversed[i]}`, { title: `${reversed[i]}입니다` });
+//   });
+// }
+
+//multer 페이지
+app.get("/multertest", (req, res) => {
+  res.render("multertest");
 });
-//버튼
-for (let i = 0; i < 20; i++) {
-  //console.log(file_list[i]);
-  app.get(`/${file_list[i]}`, function (req, res) {
-    res.render(`${reversed[i]}`, { title: `${reversed[i]}입니다` });
-  });
-}
+
+app.post("/upload", upload.single("files"), (req, res) => {
+  console.log(req.file, "파일");
+  console.log(req.body, "잘 담겼나");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
